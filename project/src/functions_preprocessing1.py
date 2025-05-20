09d6c680ec44d11a5c6eecb3bad8e11c9e953a26
sinarollin <sina.rollin@epfl.ch>
2025-05-20 10:09:54 +0200

    update to the report for processing part 1 plus corresponding functions

--- project/src/functions_preprocessing1.py
+++ project/src/functions_preprocessing1.py
@@ -16,8 +16,8 @@ def plot_rgb_distribution(color_data):
 
     Parameters
     ----------
-    color_data : dict
-        A dictionary containing color names and their corresponding RGB values.
+    color_data : The chocolates and their corresponding RGB values.
+
     """
 
     _, ax = plt.subplots(figsize=(12, 8))
@@ -56,33 +56,30 @@ def rgb_to_hsv(rgb):
     
     Parameters
     ----------
-    rgb : tuple
-        A tuple of three integers representing the RGB color.
+    rgb : The RGB color.
         
     Returns
     -------
-    hsv : tuple
-        A tuple of three integers representing the HSV color.
+    hsv :The HSV color.
     """
     color = np.uint8([[rgb]])
     hsv = cv2.cvtColor(color, cv2.COLOR_RGB2HSV)
     return hsv[0][0]
 
 
-# Function to create a combined mask in RGB
-def create_combined_mask_rgb(image_rgb, color_data, tolerance=(30, 30, 30)):
+def create_combined_mask_rgb(image_rgb, color_data, tolerance=(30, 30, 30), kernel_fill=30, kernel_open=25):
     """
     Create a combined mask in RGB color space.
 
     Parameters
     ----------
-    image_rgb: Input image in RGB format
-    color_data: Dictionary of reference colors
-    tolerance: Tolerance for color matching (R, G, B)
+    image_rgb: Input image in RGB format.
+    color_data: Dictionary of reference colors.
+    tolerance: Tolerance for color matching (R, G, B).
 
     Returns
     -------
-    Combined mask
+    Combined mask.
     """
     combined_mask = np.zeros(image_rgb.shape[:2], dtype=np.uint8)
 
@@ -92,15 +89,14 @@ def create_combined_mask_rgb(image_rgb, color_data, tolerance=(30, 30, 30)):
             lower = np.array([max(c - t, 0) for c, t in zip(rgb, tolerance)], dtype=np.uint8)
             upper = np.array([min(c + t, 255) for c, t in zip(rgb, tolerance)], dtype=np.uint8)
 
-            # Create a mask for the current color
             mask = cv2.inRange(image_rgb, lower, upper)
 
             # Combine the mask with the overall mask
             combined_mask = cv2.bitwise_or(combined_mask, mask)
     
     # # Apply morphological operations to fill small holes and remove noise
-    kernel_fill = np.ones((30, 30), np.uint8)
-    kernel_open = np.ones((25, 25), np.uint8)
+    kernel_fill = np.ones((kernel_fill, kernel_fill), np.uint8)
+    kernel_open = np.ones((kernel_open, kernel_open), np.uint8)
     combined_mask = cv2.morphologyEx(combined_mask, cv2.MORPH_CLOSE, kernel_fill)  # Fill small holes
     combined_mask = cv2.morphologyEx(combined_mask, cv2.MORPH_OPEN, kernel_open)   # Remove noise
 
@@ -131,66 +127,59 @@ def create_combined_mask_hsv(image_bgr, color_data, tolerance=(5, 40, 30), kerne
     hsv_image = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2HSV)
     combined_mask = np.zeros(hsv_image.shape[:2], dtype=np.uint8)
 
-    for reference_name, rgb_colors in color_data.items():
+    for _, rgb_colors in color_data.items():
         for rgb in rgb_colors:
             hsv = rgb_to_hsv(rgb)
             lower = np.array([
-                max(hsv[0] - tolerance[0], 0),
-                max(hsv[1] - tolerance[1], 0),
-                max(hsv[2] - tolerance[2], 0)
-            ], dtype=np.uint8)  # Explicitly cast to uint8
+                max(int(hsv[0]) - tolerance[0], 0),
+                max(int(hsv[1]) - tolerance[1], 0),
+                max(int(hsv[2]) - tolerance[2], 0)
+            ], dtype=np.uint8)
             upper = np.array([
-                min(hsv[0] + tolerance[0], 255),
-                min(hsv[1] + tolerance[1], 255),
-                min(hsv[2] + tolerance[2], 255)
-            ], dtype=np.uint8)  # Explicitly cast to uint8
+                min(int(hsv[0]) + tolerance[0], 255),
+                min(int(hsv[1]) + tolerance[1], 255),
+                min(int(hsv[2]) + tolerance[2], 255)
+            ], dtype=np.uint8)
             mask = cv2.inRange(hsv_image, lower, upper)
             combined_mask = cv2.bitwise_or(combined_mask, mask)
 
-    # Apply morphological operations
-    # kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    #Morphological operations
     kernel_fill = np.ones((kernel_fill, kernel_fill), np.uint8)
     kernel_open = np.ones((kernel_open, kernel_open), np.uint8)
     combined_mask = cv2.morphologyEx(combined_mask, cv2.MORPH_CLOSE, kernel_fill)  # Fill small holes
     combined_mask = cv2.morphologyEx(combined_mask, cv2.MORPH_OPEN, kernel_open)   # Remove noise
 
-
     return combined_mask
 
 
-# Function to create a combined mask in RGB
-def create_combined_mask_rgb(image_rgb, color_data, tolerance=(30, 30, 30)):
+
+def classify_group(mean_rgb, mean_hsv, dist):
     """
-    Create a combined mask in RGB color space.
+    Classifies the group of the object based on its mean RGB, its HSV values and its 
+    distance to a reference colour.
 
     Parameters
     ----------
-    image_rgb: Input image in RGB format
-    color_data: Dictionary of reference colors
-    tolerance: Tolerance for color matching (R, G, B)
+    mean_rgb : The mean RGB values.
+    mean_hsv : The mean HSV values.
+    dist : The distance to a reference colour.
 
     Returns
     -------
-    return: Combined mask
+    The group of the object.
     """
-    combined_mask = np.zeros(image_rgb.shape[:2], dtype=np.uint8)
 
-    for _, rgb_colors in color_data.items():
-        for rgb in rgb_colors:
-            # Define lower and upper bounds for the color with tolerance
-            lower = np.array([max(c - t, 0) for c, t in zip(rgb, tolerance)], dtype=np.uint8)
-            upper = np.array([min(c + t, 255) for c, t in zip(rgb, tolerance)], dtype=np.uint8)
-
-            # Create a mask for the current color
-            mask = cv2.inRange(image_rgb, lower, upper)
-
-            # Combine the mask with the overall mask
-            combined_mask = cv2.bitwise_or(combined_mask, mask)
-    
-    # # Apply morphological operations to fill small holes and remove noise
-    kernel_fill = np.ones((30, 30), np.uint8)
-    kernel_open = np.ones((25, 25), np.uint8)
-    combined_mask = cv2.morphologyEx(combined_mask, cv2.MORPH_CLOSE, kernel_fill)  # Fill small holes
-    combined_mask = cv2.morphologyEx(combined_mask, cv2.MORPH_OPEN, kernel_open)   # Remove noise
-
-    return combined_mask
+    #White Background
+    if mean_hsv[0] < 15 and mean_hsv[1] < 7 and dist < 116 and dist >68:
+        return "White Background"
+    #Red Jelly Bag
+    if mean_rgb[0] >168 and dist< 107 and mean_hsv[0]<8 and mean_hsv[0]> 4 and mean_hsv[1]>28:
+        return "Red Jelly Bag"
+    #Orange/Blue Book
+    if mean_hsv[0]<50 and mean_hsv[1]>27:
+        return "Orange/Blue Book"
+    #Blue Book
+    if dist < 108 and dist> 78 and mean_hsv[0]> 90:
+        return "Blue Book"
+    #Other
+    return "Other"
